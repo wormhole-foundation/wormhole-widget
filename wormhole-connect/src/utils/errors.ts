866d16603995cfc3c8d399a171264f49ec490cc8
Artur Sapek <art@wormholelabs.xyz>
2024-10-28 10:31:55 -0400

    catch "plugin closed" for wallet being closed

--- wormhole-connect/src/utils/errors.ts
+++ wormhole-connect/src/utils/errors.ts
@@ -15,7 +15,7 @@ import { Chain } from '@wormhole-foundation/sdk';
 // attempt to capture errors using regex
 export const INSUFFICIENT_ALLOWANCE_REGEX = /insufficient token allowance/im;
 export const USER_REJECTED_REGEX = new RegExp(
-  'user rejected|rejected the request|rejected from user|user cancel|aborted by user',
+  'user rejected|rejected the request|rejected from user|user cancel|aborted by user|plugin closed',
   'mi',
 );
 
