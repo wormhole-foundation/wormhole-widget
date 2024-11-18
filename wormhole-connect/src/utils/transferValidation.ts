wormhole-foundation/wormhole-connect

use amount.Amount type to store balances

wormhole-connect/src/utils/transferValidation.ts
@@ -18,7 +18,8 @@ import { walletAcceptedChains } from './wallet';
 import { useDispatch, useSelector } from 'react-redux';
 import { useDebounce } from 'use-debounce';
 import { DataWrapper } from 'store/helpers';
-import { Chain } from '@wormhole-foundation/sdk';
+import { Chain, amount as sdkAmount } from '@wormhole-foundation/sdk';
+import { getTokenDecimals } from 'utils';
 
 export const validateFromChain = (chain: Chain | undefined): ValidationErr => {
   if (!chain) return 'Select a source chain';
@@ -92,19 +93,32 @@ export const validateDestToken = (
 
 export const validateAmount = (
   amount: string,
-  balance: string | null,
-  maxAmount: number,
+  balance: sdkAmount.Amount | null,
+  fromChain: Chain | undefined,
+  token: string,
 ): ValidationErr => {
   if (amount === '') return '';
+
   const numAmount = Number.parseFloat(amount);
   if (isNaN(numAmount)) return 'Amount must be a number';
-  if (numAmount <= 0) return 'Amount must be greater than 0';
-  if (balance) {
-    const b = Number.parseFloat(balance.replaceAll(',', ''));
-    if (numAmount > b) return 'Amount exceeds available balance.';
-  }
-  if (numAmount > maxAmount) {
-    return `At the moment, amount cannot exceed ${maxAmount}`;
+
+  if (fromChain && token) {
+    // If user has selected chain, token, and has a balance entry, we can compare
+    // their amount input to their balance (using base units)
+    const tokenConfig = config.tokens[token];
+    const decimals = getTokenDecimals(fromChain, tokenConfig.tokenId);
+    const amountInput = sdkAmount.parse(amount, decimals);
+    const amountBaseUnits = sdkAmount.units(amountInput);
+    if (amountBaseUnits === 0n) {
+      return 'Amount must be greater than 0';
+    }
+
+    if (balance) {
+      const balanceBaseUnits = sdkAmount.units(balance);
+      if (amountBaseUnits > balanceBaseUnits) {
+        return 'Amount exceeds available balance';
+      }
+    }
   }
   return '';
 };
@@ -159,13 +173,6 @@ export const validateReceiveAmount = (
   return '';
 };
 
-export const getMaxAmt = (route?: string): number => {
-  if (!route) return Infinity;
-  const r = config.routes.get(route);
-  if (!r) return Infinity;
-  return r.getMaxSendAmount();
-};
-
 export const getIsAutomatic = (route?: string): boolean => {
   if (!route) return false;
   const r = config.routes.get(route);
@@ -197,7 +204,6 @@ export const validateAll = async (
     fromChain,
     token,
   );
-  const maxSendAmount = getMaxAmt(route);
   const baseValidations = {
     sendingWallet: await validateWallet(sending, fromChain),
     receivingWallet: await validateWallet(receiving, toChain),
@@ -208,7 +214,8 @@ export const validateAll = async (
     amount: validateAmount(
       amount,
       sendingTokenBalance?.balance || null,
-      maxSendAmount,
+      fromChain,
+      token,
     ),
     toNativeToken: '',
     relayerFee: '',

wormhole-foundation/wormhole-widget

wormhole-connect/src/utils/transferValidation.ts
@@ -17,7 +17,7 @@ import { RelayState } from 'store/relay';
 import { walletAcceptedChains } from './wallet';
 import { useDispatch, useSelector } from 'react-redux';
 import { useDebounce } from 'use-debounce';
-import { Chain } from '@wormhole-foundation/sdk';
+import { Chain, amount as sdkAmount } from '@wormhole-foundation/sdk';
 
 export const validateFromChain = (chain: Chain | undefined): ValidationErr => {
   if (!chain) return 'Select a source chain';
@@ -90,16 +90,23 @@ export const validateDestToken = (
 };
 
 export const validateAmount = (
-  amount: string,
-  balance: string | null,
+  amount: sdkAmount.Amount | undefined,
+  balance: sdkAmount.Amount | null,
 ): ValidationErr => {
-  if (amount === '') return '';
-  const numAmount = Number.parseFloat(amount);
-  if (isNaN(numAmount)) return 'Amount must be a number';
-  if (numAmount <= 0) return 'Amount must be greater than 0';
+  if (!amount) return '';
+
+  // If user has selected chain, token, and has a balance entry, we can compare
+  // their amount input to their balance (using base units)
+  const amountBaseUnits = sdkAmount.units(amount);
+  if (amountBaseUnits === 0n) {
+    return 'Amount must be greater than 0';
+  }
+
   if (balance) {
-    const b = Number.parseFloat(balance.replaceAll(',', ''));
-    if (numAmount > b) return 'Amount exceeds available balance.';
+    const balanceBaseUnits = sdkAmount.units(balance);
+    if (amountBaseUnits > balanceBaseUnits) {
+      return 'Amount exceeds available balance';
+    }
   }
   return '';
 };
@@ -113,7 +120,7 @@ export const validateWallet = async (
 ): Promise<ValidationErr> => {
   if (!wallet.address) return 'Wallet not connected';
   try {
-    const isSanctioned = await checkAddressIsSanctioned(wallet.address);
+    const isSanctioned = checkAddressIsSanctioned(wallet.address);
     if (isSanctioned)
       return 'This address is sanctioned, bridging is not available';
   } catch (e) {
@@ -227,8 +234,7 @@ export const validate = async (
     transferInput.toChain &&
     transferInput.token &&
     transferInput.destToken &&
-    transferInput.amount &&
-    Number.parseFloat(transferInput.amount) >= 0;
+    transferInput.amount;
 
   if (!isCanceled()) {
     dispatch(