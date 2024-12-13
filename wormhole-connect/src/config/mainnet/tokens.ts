a3614b3d25103f97507bf8302252bfc5e2d906db
Kevin Peters <kevin@wormholelabs.xyz>
2024-12-13 10:59:05 -0600

    fix usdc icon

--- wormhole-connect/src/config/mainnet/tokens.ts
+++ wormhole-connect/src/config/mainnet/tokens.ts
@@ -402,7 +402,7 @@ export const MAINNET_TOKENS: TokensConfig = {
       address:
         '0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC',
     },
-    icon: Icon.USDC,
+    icon: TokenIcon.USDC,
     coinGeckoId: 'usd-coin',
     color: '#2774CA',
     decimals: 6,
--- wormhole-connect/src/config/testnet/tokens.ts
+++ wormhole-connect/src/config/testnet/tokens.ts
@@ -187,7 +187,7 @@ export const TESTNET_TOKENS: TokensConfig = {
       address:
         '0xa1ec7fc00a6f40db9693ad1415d0c193ad3906494428cf252621037bd7117e29::usdc::USDC',
     },
-    icon: Icon.USDC,
+    icon: TokenIcon.USDC,
     coinGeckoId: 'usd-coin',
     color: '#2774CA',
     decimals: 6,
