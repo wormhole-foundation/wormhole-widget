99723994ad9845293aadebbd41826a899ea0e5a0
Alexander <alexyammine94@gmail.com>
2024-12-13 11:32:36 -0400

    Add new DW configs for MAINNET and TESTNET networks.

--- wormhole-connect/src/utils/dynamic-wallet/DynamicContext.tsx
+++ wormhole-connect/src/utils/dynamic-wallet/DynamicContext.tsx
@@ -4,6 +4,7 @@ import { EthereumWalletConnectors } from "@dynamic-labs/ethereum"
 import { SolanaWalletConnectors } from "@dynamic-labs/solana"
 import { OnConnectCallback } from "./useDynamicWallet"
 import { Theme } from "@mui/material"
+import config from "config"
 
 interface ConfiguredDynamicContextProps {
     theme?: Theme
@@ -13,18 +14,21 @@ interface ConfiguredDynamicContextProps {
 export const ConfiguredDynamicContext: React.FC<PropsWithChildren<ConfiguredDynamicContextProps>> = (
     { children, onConnectCallbackRef }
 ) => {
+    const environmentId = config.isMainnet ? "12430fd8-3f08-42d3-9b7c-7c70c66535b3" : "b1f4a038-1092-4656-b91d-e61648740572"
     return <Fragment>
         <DynamicContextProvider
             // TODO: Config theme
             // theme={ theme ?? { mode: "dark" } }
             theme={"dark"}
             settings={{
-                environmentId: "4d965b25-755b-4e7f-bfae-14cf0e2613b0",
+                environmentId,
+                enableVisitTrackingOnConnectOnly: false,
                 initialAuthenticationMode: "connect-only",
                 walletConnectors: [
                     EthereumWalletConnectors,
                     SolanaWalletConnectors,
                 ],
+                networkValidationMode: "never",
                 events: {
                     onAuthSuccess: ({ primaryWallet }) => {
                         console.log("onAuthSuccess", primaryWallet?.chain, primaryWallet?.address, primaryWallet?.connector.name)
