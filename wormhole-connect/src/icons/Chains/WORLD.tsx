84c171f514546b50a25631fd2256edd7ccdc2195
kev1n-peters <96065607+kev1n-peters@users.noreply.github.com>
2024-12-17 08:25:57 -0500

    sui: manual cctp support (#3074)

--- wormhole-connect/src/icons/Chains/WORLD.tsx
+++ wormhole-connect/src/icons/Chains/WORLD.tsx
@@ -13,23 +13,23 @@ function WORLD() {
       <path
         d="M29.1239 11.0384L18.375 11.0554C14.8158 11.0554 11.9303 13.9408 11.9303 17.5C11.9303 21.0592 14.8158 23.9447 18.375 23.9447H28.726"
         stroke="white"
-        stroke-width="2.25944"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
+        strokeWidth="2.25944"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
       />
       <path
         d="M6.17065 17.5H31.0001"
         stroke="white"
-        stroke-width="2.25944"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
+        strokeWidth="2.25944"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
       />
       <path
         d="M18.5 5C25.4036 5 31 10.5964 31 17.5C31 24.4036 25.4036 30 18.5 30C11.5964 30 6 24.4036 6 17.5C6 10.5964 11.5964 5 18.5 5Z"
         stroke="white"
-        stroke-width="2.25944"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
+        strokeWidth="2.25944"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
       />
     </svg>
   );
