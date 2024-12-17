wormholelabs-xyz/wormhole-connect

sui: manual cctp support

wormhole-connect/src/config/mainnet/tokens.ts
@@ -393,6 +393,20 @@ export const MAINNET_TOKENS: TokensConfig = {
     color: '#8457EF',
     decimals: 9,
   },
+  USDCsui: {
+    key: 'USDCsui',
+    symbol: 'USDC',
+    nativeChain: 'Sui',
+    tokenId: {
+      chain: 'Sui',
+      address:
+        '0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC',
+    },
+    icon: Icon.USDC,
+    coinGeckoId: 'usd-coin',
+    color: '#2774CA',
+    decimals: 6,
+  },
   APT: {
     key: 'APT',
     symbol: 'APT',

wormhole-connect/src/config/testnet/tokens.ts
@@ -178,6 +178,20 @@ export const TESTNET_TOKENS: TokensConfig = {
     color: '#8457EF',
     decimals: 9,
   },
+  USDCsui: {
+    key: 'USDCsui',
+    symbol: 'USDC',
+    nativeChain: 'Sui',
+    tokenId: {
+      chain: 'Sui',
+      address:
+        '0xa1ec7fc00a6f40db9693ad1415d0c193ad3906494428cf252621037bd7117e29::usdc::USDC',
+    },
+    icon: Icon.USDC,
+    coinGeckoId: 'usd-coin',
+    color: '#2774CA',
+    decimals: 6,
+  },
   APT: {
     key: 'APT',
     symbol: 'APT',

wormhole-foundation/wormhole-widget

wormhole-connect/src/config/mainnet/tokens.ts
@@ -393,6 +393,20 @@ export const MAINNET_TOKENS: TokensConfig = {
     color: '#8457EF',
     decimals: 9,
   },
+  USDCsui: {
+    key: 'USDCsui',
+    symbol: 'USDC',
+    nativeChain: 'Sui',
+    tokenId: {
+      chain: 'Sui',
+      address:
+        '0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC',
+    },
+    icon: TokenIcon.USDC,
+    coinGeckoId: 'usd-coin',
+    color: '#2774CA',
+    decimals: 6,
+  },
   APT: {
     key: 'APT',
     symbol: 'APT',

wormhole-connect/src/config/testnet/tokens.ts
@@ -178,6 +178,20 @@ export const TESTNET_TOKENS: TokensConfig = {
     color: '#8457EF',
     decimals: 9,
   },
+  USDCsui: {
+    key: 'USDCsui',
+    symbol: 'USDC',
+    nativeChain: 'Sui',
+    tokenId: {
+      chain: 'Sui',
+      address:
+        '0xa1ec7fc00a6f40db9693ad1415d0c193ad3906494428cf252621037bd7117e29::usdc::USDC',
+    },
+    icon: TokenIcon.USDC,
+    coinGeckoId: 'usd-coin',
+    color: '#2774CA',
+    decimals: 6,
+  },
   APT: {
     key: 'APT',
     symbol: 'APT',